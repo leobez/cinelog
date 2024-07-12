fdc073a9dda5210bb5cd6da8fe58106ff2adc7e1
leobez <leossbezerra@gmail.com>
2024-07-12 13:06:33 -0300

    progress

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,10 +1,7 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import MovieCard from '../components/MovieCard'
 import MovieContext, { MovieContextType } from '../context/MovieContext'
 import ImageSlider from '../components/Imageslider'
-
-const POSTER_URL = import.meta.env.VITE_POSTER_URL
 
 const Movie = () => {
     
@@ -12,7 +9,6 @@ const Movie = () => {
 
     const [movie, setMovie] = useState<any>(null)
     const [similarMovies, setSimilarMovies] = useState<any[]|null>(null)
-    const [similarMoviesPoster, setSimilarMoviesPosters] = useState<any[]|null>(null) //
     const {GET_movie_byid, GET_movies_similar} = useContext(MovieContext) as MovieContextType
 
     useEffect(() => {
@@ -26,14 +22,6 @@ const Movie = () => {
             const tempList = await GET_movies_similar(Number(id))
             if (!tempList) return;
             setSimilarMovies(tempList)
-
-            const tempPosterList:any[] = []
-
-            tempList.map((movie:any) => {
-                tempPosterList.push(`${POSTER_URL}/${movie.poster_path}`)
-            })
-            setSimilarMoviesPosters(tempPosterList)
-            console.log('tempPosterList: ', tempPosterList)
           }
           
           ASYNC_GET_movie_byid_and_similar()
@@ -58,13 +46,13 @@ const Movie = () => {
     if (movie) {
 
         return (
-            <div className='flex justify-center flex-col scrollbar-thin border-color05 gap-4'>
+            <div className='flex justify-center flex-col scrollbar-thin border-color05 gap-4 overflow-hidden'>
                 
                 {/* MOVIE */}
                 <div className='flex justify-center gap-4 '>
 
                     {/* POSTER */}
-                    <div className='flex w-3/6 max-h-[700px] border-color05 border-2'>
+                    <div className='flex w-3/6 min-h-full border-color05 border-2'>
                         <img src={`${import.meta.env.VITE_POSTER_URL}/${movie.poster_path}`} alt={movie.title} className='h-full w-full object-cover' />
                     </div>
 
@@ -134,66 +122,70 @@ const Movie = () => {
                         
                         {/* PRODUCTION DETAILS */}
                         <div>
-                            <button className='border-2 border-color05 w-full p-2 hover:bg-color05 hover:text-white animate-in' onClick={toggleProdDetail}>Production details</button>
-                            <div className='hidden border-2 border-color05 p-2 text-left animate-in z-10' id='prod-detail' ref={prodDetail}>
-                                <table className="table">
-                                    <tbody>
-                                    {/* row 1 */}
-                                    <tr>
-                                        <td>Budget</td>
-                                        <td className='font-bold'>{movie.budget}</td>
-                                    </tr>
+                            <button className='border-2 border-color05 w-full p-2 hover:bg-color05 hover:text-white' onClick={toggleProdDetail}>
+                                Production details
+                            </button>
+                            <div className='relative'>
+                                <div className='h-72 w-full hidden top-0 border-2 border-color05 p-2 text-left animate-in -translate-x-full duration-400 absolute -right-full z-20 bg-white overflow-y-auto scrollbar-thin' id='prod-detail' ref={prodDetail} >
+                                    <table className="table">
+                                        <tbody>
+                                        {/* row 1 */}
+                                        <tr>
+                                            <td>Budget</td>
+                                            <td className='font-bold'>{movie.budget}</td>
+                                        </tr>
 
-                                    {/* row 2 */}
-                                    <tr>
-                                        <td>Revenue</td>
-                                        <td className='font-bold'>{movie.revenue}</td>
-                                    </tr>
+                                        {/* row 2 */}
+                                        <tr>
+                                            <td>Revenue</td>
+                                            <td className='font-bold'>{movie.revenue}</td>
+                                        </tr>
 
-                                    {/* row 3 */}
-                                    <tr>
-                                        <td>Origin country</td>
-                                        <td className='font-bold'>
-                                            {movie.origin_country.map((country:string, index:number) => (
-                                                <span key={index}>{country} </span>
+                                        {/* row 3 */}
+                                        <tr>
+                                            <td>Origin country</td>
+                                            <td className='font-bold'>
+                                                {movie.origin_country.map((country:string, index:number) => (
+                                                    <span key={index}>{country} </span>
+                                                ))}
+                                            </td>
+                                        </tr>
+
+                                        {/* row 4 */}
+                                        <tr>
+                                            <td>Original Language</td>
+                                            <td className='font-bold'>{movie.original_language}</td>
+                                        </tr>
+
+                                        {/* row 5 */}
+                                        <tr>
+                                            <td>Production companies</td>
+                                            <td className='font-bold'>
+                                                {movie.production_companies.map((compan:any) => (
+                                                    <div key={compan.id}>
+                                                        <div>{compan.name} [{compan.origin_country}]</div>
+                                                        {/* <img src={`${import.meta.env.VITE_POSTER_URL}/${compan.logo_path}`} alt={compan.name} className='h-12 w-12 object-contain'/> */}
+                                                    </div>
                                             ))}
-                                        </td>
-                                    </tr>
+                                            </td>
+                                        </tr>
 
-                                    {/* row 4 */}
-                                    <tr>
-                                        <td>Original Language</td>
-                                        <td className='font-bold'>{movie.original_language}</td>
-                                    </tr>
+                                        {/* row 6 */}
+                                        <tr>
+                                            <td>Production countries</td>
+                                            <td className='font-bold'>
+                                                {movie.production_countries.map((country:any, index:number) => (
+                                                    <div key={index}>
+                                                        <div>{country.name} [{country.iso_3166_1}]</div>
+                                                    </div>
+                                            ))}
+                                            </td>
+                                        </tr>   
 
-                                    {/* row 5 */}
-                                    <tr>
-                                        <td>Production companies</td>
-                                        <td className='font-bold'>
-                                            {movie.production_companies.map((compan:any) => (
-                                                <div key={compan.id}>
-                                                    <div>{compan.name} [{compan.origin_country}]</div>
-                                                    {/* <img src={`${import.meta.env.VITE_POSTER_URL}/${compan.logo_path}`} alt={compan.name} className='h-12 w-12 object-contain'/> */}
-                                                </div>
-                                        ))}
-                                        </td>
-                                    </tr>
+                                        </tbody>
 
-                                    {/* row 6 */}
-                                    <tr>
-                                        <td>Production countries</td>
-                                        <td className='font-bold'>
-                                            {movie.production_countries.map((country:any, index:number) => (
-                                                <div key={index}>
-                                                    <div>{country.name} [{country.iso_3166_1}]</div>
-                                                </div>
-                                        ))}
-                                        </td>
-                                    </tr>   
-
-                                    </tbody>
-
-                                </table>
+                                    </table>
+                                </div>
                             </div>
                         </div>
 
